4e741318cca1d32f7c73d16ec283a70391c1547e
Adam Love <adam.love127@gmail.com>
2015-02-04 13:18:18 +1100

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,8 @@ class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
 		# render 'visitors/new'
-		flash.now[:notice] = 'Welcome!'
-		flash.now[:alert] = 'My birthday is soon'
+		#flash.now[:notice] = 'Welcome!'
+		#flash.now[:alert] = 'My birthday is soon'
 		#Flash messages
 		# - use flash.now when rendering a page
 		# - use flash in the controller when redirecting to another page
